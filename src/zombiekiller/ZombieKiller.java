3f8d74ca64365887904140a98f80dca61b382065
therandomfool <mddenver@protonmail.com>
2020-08-02 22:03:38 -0700

    basic array revist

---
+++ src/zombiekiller/ZombieKiller.java
@@ -0,0 +1,15 @@
+package zombiekiller;
+
+public class ZombieKiller {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to our zombie killer program!!!");
+		
+		
+		String backpack[] = {"Shotgun", "Laser Rifle", "Sniper Rifle"};
+		String zombies[] = {"Close Range Zombie", "Mid-Range Zombie", "Long-Range Zombie"};
+		
+		
+	}
+	
+}
